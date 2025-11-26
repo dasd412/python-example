8e47b0792f7ca137366c2419ddfece49524e51d4
yang-yeongjun <dasd412@naver.com>
2025-11-26 12:01:33 +0900

    람다

---
+++ method/lambda.py
@@ -0,0 +1,15 @@
+def main():
+    add=lambda x,y:x+y
+    result=add(3,5)
+    print(result)
+
+    numbers=[1,2,3,4,5]
+    squared=list(map(lambda x:x**2, numbers))
+    print(squared)
+
+    numbers=[i for i in range(0,11)]
+    even_numbers=list(filter(lambda x:x%2==0,numbers))
+    print(even_numbers)
+
+if __name__ == "__main__":
+    main()
