fac5b69a7fbc0dfe74e35c4a52890749bf04dc3b
dasd <dasd412@naver.com>
2026-01-03 14:29:17 +0900

    커넥션

--- connection-pool/connection.py
+++ connection-pool/connection.py
@@ -1,12 +1,19 @@
+import asyncio
+
 
 class Connection:
 
-    def __init__(self,conn_id:int):
-        self.conn_id=conn_id
-        self.is_open=True
+    def __init__(self, conn_id: int):
+        self.conn_id = conn_id
+        self.is_open = True
 
-    async def execute(self,query:str)->str:
-        pass
+    async def execute(self, query: str) -> str:
+        if self.is_open:
+            await asyncio.sleep(1)
+            print("query : ", query)
+            return "execute success"
+        else:
+            return "execute failed"
 
     async def close(self):
-        pass
+        self.is_open = False
